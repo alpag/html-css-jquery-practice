ff11eeee9c9203c1c426b53995121cf8cce42c62
Marcin Pawelski <marcinpawelski.96@gmail.com>
2018-12-26 13:40:55 +0100

    CSS and HTML basics

---
+++ myscript.js
@@ -0,0 +1,10 @@
+console.log("PAWELSKI PROGRAMISTA 15K SOON");
+
+$(document).ready(function(){
+    
+
+    $("#button-C").click(function(){
+        console.log("XDD");
+        $("#screen").empty();
+    })
+});
