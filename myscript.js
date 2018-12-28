a4f0a740b710cd9860e777ed6bb4caac7a982c49
Marcin Pawelski <marcinpawelski.96@gmail.com>
2018-12-28 11:50:42 +0100

    Jquery functionality, working buttons

--- myscript.js
+++ myscript.js
@@ -1,10 +1,55 @@
 console.log("PAWELSKI PROGRAMISTA 15K SOON");
 
 $(document).ready(function(){
-    
+    var currentNumber = '';
+    var previousNumber = '';
+    var operation;
 
     $("#button-C").click(function(){
-        console.log("XDD");
-        $("#screen").empty();
+        $("#screen").empty().append("0");
+        currentNumber = '';
+        previousNumber = '';
+        equals();
     })
+
+    $("#button-previous").click(function(){
+        if(currentNumber.length > 1)
+            currentNumber = currentNumber.substr(0, currentNumber.length-1);
+        else
+            currentNumber = 0;
+        $("#screen").empty().append(currentNumber);
+    })
+
+    $(".digit-clickable").click(function(){
+        currentNumber+=$(this).text();
+        $("#screen").empty().append(currentNumber);
+    })
+
+    $(".action-clickable").click(function(){
+        equals();
+        $("#screen").empty().append("0");
+        previousNumber = currentNumber;
+        currentNumber = '';
+        operation = $(this).text();
+    })
+    function equals(){
+        $(".equals").click(function(){
+            switch(operation){
+                case "+":
+                    currentNumber = parseInt(previousNumber) + parseInt(currentNumber);
+                    break;
+                case "-":
+                    currentNumber = parseInt(previousNumber) - parseInt(currentNumber);
+                    break;
+                case "/":
+                    currentNumber = parseInt(previousNumber) / parseInt(currentNumber);            
+                    break;
+                case "x":
+                    currentNumber = parseInt(previousNumber) * parseInt(currentNumber);
+            }
+            $("#screen").empty().append(currentNumber);
+            $(".equals").off();
+        })
+    }
+    equals();
 });
